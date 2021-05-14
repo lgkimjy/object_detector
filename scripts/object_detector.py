6ccb75bd0a74e7644f05afcdfc389a60286aa476
lgkimjy <lgkimjy@naver.com>
2021-05-14 17:06:52 +0900

    obstacle clustering

--- scripts/object_detector.py
+++ scripts/object_detector.py
@@ -3,14 +3,19 @@ import rospy
 from std_msgs.msg import String
 from sensor_msgs.msg import LaserScan
 
-def scan_callback(data):
+thres_shortest = 1.0 # units: m
+thres_obj_gap = 0.2  # units: m
+obs_max_size = 50    # units : obstacle laser scan points
+
+obs_msg = LaserScan()
+
+def LaserHandler(data):
     
-    thres_shortest = 1.0 # units: m
-    thres_obj_gap = 0.1  # units: m
     shortest = 0
     shortest_idx = 0
     shortest_flag = False
 
+    # FIND SHORTEST RANGES IDX
     for i in range(len(data.ranges)):
         if(data.ranges[i] == 0):
             pass
@@ -18,14 +23,35 @@ def scan_callback(data):
             shortest = data.ranges[i]
             shortest_idx = i
             shortest_flag = True
-    print(shortest_flag, " ", shortest_idx)
+    # print(shortest_flag, " ", shortest_idx)
 
-def listener():
+    # CLUSTERING
+    left_idx = right_idx = shortest_idx
+    left_flag = right_flag = True
+    obs_msg.ranges[shortest_idx] = data.ranges[shortest_idx]
+    obs_msg.header.frame_id = "laser_frame"
+    for i in range(obs_max_size):
+
+        if(data.ranges[left_idx] - data.ranges[left_idx + i] < thres_obj_gap and left_flag == True):
+            left_idx = left_idx + i
+            obs_msg.ranges[left_idx] = data.ranges[left_idx]
+        else:
+            left_flag = False
+
+        if(data.ranges[right_idx] - data.ranges[right_idx - i] < thres_obj_gap and right_flag == True):
+            right_idx = right_idx + i
+            obs_msg.ranges[right_idx] = data.ranges[right_idx]
+        else:
+            right_flag = False
+
+if __name__ == '__main__':
     
     rospy.init_node('object_detector', anonymous=True)
 
-    rospy.Subscriber("/scan", LaserScan, scan_callback)
-    rospy.spin()
+    pub = rospy.Publisher("/closest_obstacle", LaserScan, anonymous=True)
 
-if __name__ == '__main__':
-    listener()
+    rospy.Subscriber("/scan", LaserScan, LaserHandler)
+
+    while not rospy.is_shutdown():
+        pub.publish(obs_msg)
+    rospy.spin()
